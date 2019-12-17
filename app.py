8e188baf652ee5479edff36b6cbc824267108a2e
Swapnil Vijay <swapnilvijay9@gmail.com>
2019-12-17 12:22:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-
-# In[4]:
-
-
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-
-
-# In[5]:
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-# In[6]:
-
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -36,14 +18,10 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = prediction
+    output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Predicted House Price is $ {}'.format(output))
 
 
-# In[7]:
-
-
 if __name__ == "__main__":
     app.run(debug=True)
-
